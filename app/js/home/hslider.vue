405dd6ded61f5065bf0333607561b988cb72d250
xuech <xuech_love@126.com>
2018-12-04 11:40:07 +0800

    借贷和新手特权

--- app/js/home/hslider.vue
+++ app/js/home/hslider.vue
@@ -13,44 +13,43 @@
 </template>
 
 <script>
-    import Slider from "../core/slider.vue"
-
-    export default {
-        components: {
-            Slider
-        },
-        data() {
-            return {
-                items: [{
-                    href: "home",
-                    src: "//img12.360buyimg.com/jrpmobile/jfs/t29656/64/888223268/102622/5d170d4a/5c0126acN6974d9ab.png?width=750&height=320",
-                }, {
-                    href: "home",
-                    src: "//img12.360buyimg.com/jrpmobile/jfs/t27235/261/2442633687/58984/1147eb82/5c0126d1N88b5b924.png?width=750&height=320",
-                }, {
-                    href: "home",
-                    src: "//img12.360buyimg.com/jrpmobile/jfs/t26575/152/2411242031/46179/b3c7773b/5c01265cNce2a3299.jpg?width=750&height=320",
-                }],
-                enters: [{
-                    href: "download",
-                    img: "//img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132",
-                    title: "优惠券",
-                }, {
-                    href: "home",
-                    img: "//img12.360buyimg.com/jrpmobile/jfs/t5590/252/875247023/17343/946aa72c/59224650N0f7ffc92.png?width=132&height=132",
-                    title: "领红包",
-                }, {
-                    href: "home",
-                    img: "//img12.360buyimg.com/jrpmobile/jfs/t4393/329/2180608902/13217/c88c0cec/58ec9dcdN1534e2d7.png?width=132&height=132",
-                    title: "抢钢镚",
-                }, {
-                    href: "home",
-                    img: "//img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135",
-                    title: "白条提额",
-                }],
-            }
-        },
-    }
+import Slider from "../core/slider.vue"
+export default {
+    components: {
+        Slider
+    },
+    data() {
+        return {
+            items: [{
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t29656/64/888223268/102622/5d170d4a/5c0126acN6974d9ab.png?width=750&height=320",
+            }, {
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t27235/261/2442633687/58984/1147eb82/5c0126d1N88b5b924.png?width=750&height=320",
+            }, {
+                href: "home",
+                src: "//img12.360buyimg.com/jrpmobile/jfs/t26575/152/2411242031/46179/b3c7773b/5c01265cNce2a3299.jpg?width=750&height=320",
+            }],
+            enters: [{
+                href: "download",
+                img: "//img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132",
+                title: "优惠券",
+            }, {
+                href: "home",
+                img: "//img12.360buyimg.com/jrpmobile/jfs/t5590/252/875247023/17343/946aa72c/59224650N0f7ffc92.png?width=132&height=132",
+                title: "领红包",
+            }, {
+                href: "home",
+                img: "//img12.360buyimg.com/jrpmobile/jfs/t4393/329/2180608902/13217/c88c0cec/58ec9dcdN1534e2d7.png?width=132&height=132",
+                title: "抢钢镚",
+            }, {
+                href: "home",
+                img: "//img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135",
+                title: "白条提额",
+            }],
+        }
+    },
+}
 </script>
 
 <style lang="scss" module>
--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,22 @@
     <div>
         <Heador/>
         <HSlider/>
+        <Novice/>
+        <Borrow/>
     </div>
 </template>
 
 <script>
 import Heador from "../public/JDHeader.vue"
 import HSlider from "../home/hslider.vue"
+import Novice from "../home/novice.vue"
+import Borrow from "../home/borrow.vue"
 export default {
     components:{
         Heador,
-        HSlider
+        HSlider,
+        Novice,
+        Borrow
     }
 }
 </script>
