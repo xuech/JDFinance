dfbab07756ac82f66540e00afdcbc24409d4490b
xuech <xuech_love@126.com>
2018-12-04 14:06:12 +0800

    新品

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -5,6 +5,7 @@
         <Novice/>
         <Borrow/>
         <Money/>
+        <Product/>
     </div>
 </template>
 
@@ -14,13 +15,16 @@ import HSlider from "../home/hslider.vue"
 import Novice from "../home/novice.vue"
 import Borrow from "../home/borrow.vue"
 import Money from "../home/money.vue"
+import Product from "./product.vue"
+
 export default {
     components:{
         Heador,
         HSlider,
         Novice,
         Borrow,
-        Money
+        Money,
+        Product
     }
 }
 </script>
