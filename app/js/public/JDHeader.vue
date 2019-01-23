74ab17d7a44bab520314318416399ae4a6f7c4a3
xuech <xuech_love@126.com>
2019-01-23 15:04:30 +0800

    任务

--- app/js/public/JDHeader.vue
+++ app/js/public/JDHeader.vue
@@ -30,25 +30,23 @@
         background: #fff url(//m.jr.jd.com/spe/qyy/main/images/jr-logo.png) center center no-repeat;
         background-size: auto 50%;
         z-index: 100;
-
-    .left {
-        font-size: 28px;
-        height: 30px;
-        line-height: 30px;
-        margin: 17px 0 0 18px;
-    }
-
-    .btnDownload {
-        float: right;
-        font-size: 24px;
-        border-width: 0;
-        height: 56px;
-        line-height: 56px;
-        min-width: 120px;
-        padding: 0;
-        border-radius: 4px;
-        margin: 28px 24px 0 0;
-    }
+        .left {
+            font-size: 28px;
+            height: 30px;
+            line-height: 30px;
+            margin: 17px 0 0 18px;
+        }
+        .btnDownload {
+            float: right;
+            font-size: 24px;
+            border-width: 0;
+            height: 56px;
+            line-height: 56px;
+            min-width: 120px;
+            padding: 0;
+            border-radius: 4px;
+            margin: 28px 24px 0 0;
+        }
 
     }
 </style>
